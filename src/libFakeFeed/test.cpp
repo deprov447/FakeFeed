135ad2d82d81b802849c5f8be9beb78404fde2f9
Anubhav Choudhary <ac.10edu@gmail.com>
2021-04-20 08:09:42 -0400

    [added] Tests

---
+++ src/libFakeFeed/test.cpp
@@ -0,0 +1,35 @@
+#include "test.h"
+
+void
+Test::initiateTest ()
+{
+    Test t;
+    Module m;
+    t.testManageDevice( m );
+    qDebug() << "testManageDevice : passed";
+}
+
+void
+Test::testManageDevice ( Module m )
+{
+    m.manageDevice( "uvcvideo", true );
+    qDebug() << "UVCvideo module removed";
+    bool did;
+    std::cin >> did;
+    if (!did)
+        return;
+    m.manageDevice( "uvcvideo", false );
+    qDebug() << "UVCvideo module added";
+}
+
+void
+Test::testFindOriginalCam ()
+{
+
+}
+
+void
+Test::testStream ()
+{
+
+}
---
+++ src/libFakeFeed/test.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include "QDebug"
+#include "QProcess"
+#include "QStringList"
+#include "QString"
+#include "QObject"
+#include <iostream>
+
+#include <module.h>
+
+class Test {
+    public:
+        void initiateTest ();
+        void testManageDevice ( Module m );
+        void testFindOriginalCam ();
+        void testStream ();
+};
